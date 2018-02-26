459c030048226baef68e9d6f3ed90cbc7772fb81
Ben Norskov <norskovb@newschool.edu>
2018-02-25 20:52:11 -0500

    moving sass partials

--- config.rb
+++ config.rb
@@ -8,6 +8,8 @@ end
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
+ignore "/stylesheets/partials/*"
+
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
