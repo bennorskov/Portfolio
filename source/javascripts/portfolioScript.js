8d10b8799106e8c7532df7258bbcf4c8920b8ec6
Ben Norskov <norskovb@newschool.edu>
2018-02-26 00:00:58 -0500

    Antidote and Personal menu functionality. Bio inserted

--- source/javascripts/portfolioScript.js
+++ source/javascripts/portfolioScript.js
@@ -7,6 +7,12 @@ $(document).ready(function() {
 	$(".ui__navigationPulldown").on("click", function() { 
 		showNav(); 
 	});
+	$(".slideMenuNavButton__personal").on("click", function () {
+		slideMenus("personal");
+	});
+	$(".slideMenuNavButton__antidote").on("click", function () {
+		slideMenus("antidote");
+	});
 	$(".slideMenuNavButton").on("click", function () {
 		slideMenus();
 	});
@@ -14,8 +20,20 @@ $(document).ready(function() {
 
 function showNav() {
 	$(".navigation").toggleClass("collapsed");
-	$(".navigation").removeClass("showSecondaryMenu");
+	slideMenus();
 }
-function slideMenus() {
-	$(".navigation").toggleClass("showSecondaryMenu");
+function slideMenus(_type) {
+	if (_type == "personal") {
+		$(".navigation__secondaryMenu--personalMenu").show();
+		$(".navigation__secondaryMenu--antidoteMenu").hide();
+	}
+	if (_type == "antidote") {
+		$(".navigation__secondaryMenu--personalMenu").hide();
+		$(".navigation__secondaryMenu--antidoteMenu").show();
+	}
+	if (_type != null) {
+		$(".navigation").addClass("showSecondaryMenu");	
+	} else {
+		$(".navigation").removeClass("showSecondaryMenu");
+	}
 }
